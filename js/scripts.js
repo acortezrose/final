88b2b13d8f3a1d60f57d68c42c0f46ad66ed2747
acortezrose <acortezrose@gmail.com>
2019-04-17 18:43:30 -0400

    google analytics tagged

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-
-
-
 window.addEventListener('load', function(){
 	window.onscroll = function(){ stickyNav() };
 
@@ -17,6 +14,24 @@ window.addEventListener('load', function(){
 
 	var navbar = document.querySelector("#nav-wrap");
 	var sticky = navbar.offsetTop;
+
+	var acc = document.querySelectorAll(".accordion");
+	var i;
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	    /* Toggle between adding and removing the "active" class,
+	    to highlight the button that controls the panel */
+	    this.classList.toggle("active");
+
+	    /* Toggle between hiding and showing the active panel */
+	    var panel = this.nextElementSibling;
+	    if (panel.style.display === "block") {
+	      panel.style.display = "none";
+	    } else {
+	      panel.style.display = "block";
+	    }
+	  });
+	}
 
 	
 	function stickyNav(){
